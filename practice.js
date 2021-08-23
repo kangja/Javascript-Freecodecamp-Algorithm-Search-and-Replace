f2f9c5a60dd27045ec22c4be5c6517ef5e38ce72
Jason <kangja1996@gmail.com>
2021-08-23 15:44:27 -0400

    progress

--- practice.js
+++ practice.js
@@ -1,3 +1,16 @@
+// 1st solution
+// function myReplace(str, before, after) {
+//   const isCapitazlied = before[0] >= "A" && before[0] <= "Z";
+//   let casedAfter;
+//   if (isCapitazlied) {
+//     casedAfter = after[0].toUpperCase() + after.substring(1);
+//   } else {
+//     casedAfter = after[0].toLowerCase() + after.substring(1);
+//   }
+//   return str.replace(before, casedAfter);
+// }
+
+// 2nd solution
 function myReplace(str, before, after) {
   const isCapitazlied = before[0] >= "A" && before[0] <= "Z";
   let casedAfter;
@@ -6,9 +19,25 @@ function myReplace(str, before, after) {
   } else {
     casedAfter = after[0].toLowerCase() + after.substring(1);
   }
-  return str.replace(before, casedAfter);
+
+  let replacedStr = str;
+  for (let i = 0; i < replacedStr.length; i++) {
+    const iString = replacedStr.substring(i, i + before.length);
+    console.log("this is iString " + iString);
+    if (iString === before) {
+      replacedStr =
+        replacedStr.substring(0, i) +
+        casedAfter +
+        replacedStr.substring(i + before.length);
+    }
+  }
+  return replacedStr;
 }
 
 console.log(
   myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped")
 );
+
+// console.log(
+//   myReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped")
+// );
